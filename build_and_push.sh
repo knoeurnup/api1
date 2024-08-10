e95ea162497ca05663c2717f69815760fb63a208
Free Style <thecodingskill@gmail.com>
2024-08-10 15:20:32 +0700

    1.0.1

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,7 +5,7 @@ AccountID="992382804210"
 Region="us-east-1"
 ECR_Repo="$AccountID.dkr.ecr.$Region.amazonaws.com"
 VERSION="1.0.0"
-APPNAME="app3"
+APPNAME="app1"
 # aws ecr get-login --region $Region | docker login --username AWS --password-stdin $ECR_Repo
 ## Linux
 # aws ecr get-login-password --region $Region | docker login --username AWS --password-stdin $ECR_Repo
